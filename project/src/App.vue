9a01e3839260cae7e1d43e47a09bae21008ad0bc
Areeya <amyschclass@gmail.com>
2023-10-23 00:07:10 +0700

    edit default page

--- project/src/App.vue
+++ project/src/App.vue
@@ -11,22 +11,22 @@ const {message} = storeToRefs(store)
     <div id="flashMessage" v-if="message">
       <h4>{{message}}</h4>
     </div>
-    <nav class="bg-blue-600 h-32 w-full">
-        <div class="flex justify-center">
+    <nav class="bg-blue-600 h-18 w-full">
+        <div class="flex justify-left pl-5">
           <img
-            class="w-60 h-32"
+            class="w-20 h-12"
             src="https://1000logos.net/wp-content/uploads/2021/11/My-Hero-Academia-Logo.png"
             alt="Logo"
           />
         </div>
         <div class="absolute top-0 right-0 mt-2 mr-3 text-lg rounded-md bg-white px-3">
-          <RouterLink class="text-black hover:shadow-grey-300 hover:shadow-lg hover:font-bold font-mono" :to="{name:'AJ-main'}">LOGIN</RouterLink>
+          <RouterLink class="text-black hover:shadow-grey-300 hover:shadow-lg hover:font-bold font-mono" :to="{name:'login'}">LOGIN</RouterLink>
         </div>        
     </nav>
-<!--    <div class="space-x-20 text-center font-mono text-2xl pt-2 pb-5">-->
-<!--          <RouterLink class="text-black hover:shadow-grey-300 hover:shadow-lg hover:font-bold" :to="{name:'student'}">STUDENT</RouterLink>-->
-<!--          <RouterLink class="text-black hover:shadow-grey-300 hover:shadow-lg hover:font-bold" :to="{name:'advisor'}">ADVISOR</RouterLink>-->
-<!--    </div>-->
+    <div class="space-x-20 text-center font-mono text-2xl pt-2 pb-5">
+          <RouterLink class="text-black hover:shadow-grey-300 hover:shadow-lg hover:font-bold" :to="{name:'student'}">STUDENT</RouterLink>
+          <RouterLink class="text-black hover:shadow-grey-300 hover:shadow-lg hover:font-bold" :to="{name:'advisor'}">ADVISOR</RouterLink>
+    </div>
     <RouterView />
   </header>
 </template>
