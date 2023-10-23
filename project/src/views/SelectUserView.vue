4d0e8af3d6efb1a8024266bac69ed57aa8c23e92
Areeya <amyschclass@gmail.com>
2023-10-24 04:17:06 +0700

    make user select page

---
+++ project/src/views/SelectUserView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <RouterLink class="" :to="{name: 'login'}">
+    <div class="mx-auto border-2 border-indigo-600 h-50 w-52 flex flex-col items-center hover:shadow-lg hover:scale-110">
+      <h4>Student</h4>
+    </div>
+  </RouterLink>
+  <RouterLink class="" :to="{name: 'login'}">
+    <div class="mx-auto border-2 border-indigo-600 h-50 w-52 flex flex-col items-center hover:shadow-lg hover:scale-110">
+      <h4>Teacher</h4>
+    </div>
+  </RouterLink>
+  <RouterLink class="" :to="{name: 'login'}">
+    <div class="mx-auto border-2 border-indigo-600 h-50 w-52 flex flex-col items-center hover:shadow-lg hover:scale-110">
+      <h4>Admin</h4>
+    </div>
+  </RouterLink>
+</template>
+
+<style scoped>
+
+</style>
